142d51bcd939c2f759022d61efc4019d7fd38d29
Amy Shackles <amyshackles@gmail.com>
2020-06-30 14:04:09 -0700

    Added logic for \u{hhhhh}

--- components/getUnicode.js
+++ components/getUnicode.js
@@ -1,12 +1,11 @@
 module.exports = {
     getUnicode: (string, flags) => {
         const bracesPossible = flags.includes('u');
-        const unicodeRegex = bracesPossible ? /(?:\\u\{)(?<unicode>[\dabcdefABCDEF]{4})(?:\})/g : /(?:\\u)(?<unicode>[\dabcdefABCDEF]{4})/g;
+        const unicodeRegex = bracesPossible ? /(?:\\u\{)(?<unicode>[\da-fA-F]{4,5})(?:\})/g : /(?:\\u)(?<unicode>[\da-fA-F]{4})/g;
         const unicode = {};
         [...string.matchAll(unicodeRegex)].forEach((regex) => {
             let key = "unicode";
             const { groups } = regex;
-            console.log(groups);
             const startingIndex = regex.index;
             const endingIndex = startingIndex + groups[key].length;
             const codepoint = parseInt(regex.groups.unicode, 16);
