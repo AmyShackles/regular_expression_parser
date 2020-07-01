48fc78cfa4954a50df4e113583482d7044058869
Amy Shackles <amyshackles@gmail.com>
2020-06-30 18:04:20 -0700

    Cleaning up unicode property escape logic

--- components/getUnicodePropertyEscapes.js
+++ components/getUnicodePropertyEscapes.js
@@ -1,26 +1,28 @@
 module.exports = {
     getUnicodePropertyEscapes: (string) => {
-        const unicodePropertyEscapeRegex = /(?:\\\p\{)(?<unicode_name_and_value>(?<unicode_name>.*?)(?:\=)(?<unicode_value>.*?))(?:\})|(?:\\\p\{)(?<unicode_name_or_value>.*?)(?:\})|(?:\\\P\{)(?<negated_unicode_name_or_value>.*?)(?:\})/g;
+        const unicodePropertyEscapeRegex = /(?:\\\p\{)(?<unicode_property_escape>((?<unicode_name>.*?)(?:\=)(?<unicode_value>.*?))|(.*?))(?:\})|(?:\\\P\{)(?<negated_unicode_property_escape>.*?)(?:\})/g;
         const unicodePropertyEscapes = {};
         [...string.matchAll(unicodePropertyEscapeRegex)].forEach((regex) => {
             const { groups } = regex;
-            const key = groups.unicode_name_and_value? "unicode_name_and_value" : groups.unicode_name_or_value ? "unicode_name_or_value" : "negated_unicode_name_or_value"
+            const key = groups.unicode_property_escape ?  "unicode_property_escape" : "negated_unicode_property_escape";
             const startingIndex = regex.index;
             const endingIndex = startingIndex + groups[key].length;
             let group;
-            if (groups.unicode_name_and_value) {
-                group = {
-                    unicode_name: groups.unicode_name,
-                    unicode_value: groups.unicode_value,
-                    group: groups.unicode_name_and_value
-                }
-            } else if (groups.unicode_name_or_value) {
-                group = {
-                    group: groups.unicode_name_or_alue
+            if (groups.unicode_property_escape) {
+                if (groups.unicode_name) {
+                    group = {
+                        unicode_name: groups.unicode_name,
+                        unicode_value: groups.unicode_value,
+                        group: groups.unicode_property_escape
+                    }
+                } else {
+                    group = {
+                        group: groups.unicode_property_escape
+                    }
                 }
             } else {
                 group = {
-                    group: groups.negated_unicode_name_or_value
+                    group: groups.negated_unicode_property_escape
                 }
             }
             unicodePropertyEscapes[startingIndex] = {
