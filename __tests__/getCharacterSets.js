dfd8e151074b59562f9d2d5dadaaa2d6bbdddfcf
Amy Shackles <amyshackles@gmail.com>
2020-07-05 11:08:05 -0700

    Added testing to and improved getCharacterSets

--- __tests__/getCharacterSets.js
+++ __tests__/getCharacterSets.js
@@ -1,8 +1,25 @@
-// const { getCharacterSets } = require("../components/getCharacterSets.js");
-// const { NEGATED_CHARACTER_SET, CHARACTER_SET } = require('../utils/regexes.js');
-// const { splitRegex } = require("../components/splitRegex");
-// const { getIndexes } = require("../utils/getIndexes.js");
+const { getCharacterSets } = require("../components/getCharacterSets.js");
+const { NEGATED_CHARACTER_SET, CHARACTER_SET } = require('../utils/regexes.js');
+const { splitRegex } = require("../components/splitRegex");
+const { getIndexes } = require("../utils/getIndexes.js");
 
-test.todo("getCharacterSets");
-test.todo("negated character sets");
-test.todo("character sets");
+
+describe("getCharacterSets", () => {
+    const { expression, flags } = splitRegex(/[123]|[^7]]/gm)
+    const characterSets = getCharacterSets(expression, flags);
+    const negatedCharacterSetIndexes = getIndexes(expression, NEGATED_CHARACTER_SET);
+    const characterSetIndexes = getIndexes(expression, CHARACTER_SET);
+
+    it("adds negated character sets", () => {
+        expect(characterSets).toHaveProperty(`${negatedCharacterSetIndexes[0]}.negated_character_set`)
+        expect(characterSets).toHaveProperty(`${negatedCharacterSetIndexes[0]}.negated_character_set.startingIndex`, 6);
+        expect(characterSets).toHaveProperty(`${negatedCharacterSetIndexes[0]}.negated_character_set.endingIndex`, 9);
+        expect(characterSets).toHaveProperty(`${negatedCharacterSetIndexes[0]}.negated_character_set.group`, '7')
+    });
+    it("adds character sets", () => {
+        expect(characterSets).toHaveProperty(`${characterSetIndexes[0]}.character_set`)
+        expect(characterSets).toHaveProperty(`${characterSetIndexes[0]}.character_set.startingIndex`, 0);
+        expect(characterSets).toHaveProperty(`${characterSetIndexes[0]}.character_set.endingIndex`, 4);
+        expect(characterSets).toHaveProperty(`${characterSetIndexes[0]}.character_set.group`, '123')
+    })
+})
--- components/getCharacterSets.js
+++ components/getCharacterSets.js
@@ -7,11 +7,17 @@ module.exports = {
         const characterSets = {};
 
         [...string.matchAll(characterSetRegex)].forEach((regex) => {
-            let key = regex.groups.negated_character_set ? "negated_character_set" : "character_set";
             const { groups } = regex;
+            let key = groups.negated_character_set ? "negated_character_set" : "character_set";
+
             const startingIndex = regex.index;
-            const endingIndex = startingIndex + groups[key].length;
-            const group = regex.groups[key];
+            const group = groups[key]
+            // Starting index is from start of character set, not content of character set
+            // Subtract 1 from group[key].length to get last index of that range,
+            // Add two to account for brackets
+            // Add 1 more for negated character sets since the ^ character isn't included in group
+            const endingIndex = startingIndex + (group.length - 1) + (key === "negated_character_set" ? 3 : 2);
+
             characterSets[startingIndex] = {
                 [key]: {
                     startingIndex,
