1968e989072104a810483fd4eabeea92c045c5ea
Amy Shackles <amyshackles@gmail.com>
2020-07-06 22:04:12 -0700

    Fixed control character and hex

--- __tests__/getCharacterClasses.js
+++ __tests__/getCharacterClasses.js
@@ -30,165 +30,165 @@ describe("getCharacterClasses", () => {
             digitIndexes.forEach(digit => {
                 expect(characterClasses).toHaveProperty(`${digit}.digit`);
                 expect(characterClasses).toHaveProperty(`${digit}.digit.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${digit}.digit.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${digit}.digit.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${digit}.digit.group`);
             });
             expect(characterClasses).toHaveProperty(`${digitIndexes[0]}.digit.startingIndex`, 28);
-            expect(characterClasses).toHaveProperty(`${digitIndexes[0]}.digit.endingIndex`, 30);
+            expect(characterClasses).toHaveProperty(`${digitIndexes[0]}.digit.lastIndex`, 29);
             expect(characterClasses).toHaveProperty(`${digitIndexes[0]}.digit.group`, '\\d');
         });
         it("should add non-digits", () => {
             nonDigitIndexes.forEach(nonDigit => {
                 expect(characterClasses).toHaveProperty(`${nonDigit}.non_digit`);
                 expect(characterClasses).toHaveProperty(`${nonDigit}.non_digit.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${nonDigit}.non_digit.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${nonDigit}.non_digit.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${nonDigit}.non_digit.group`);
             });
             expect(characterClasses).toHaveProperty(`${nonDigitIndexes[0]}.non_digit.startingIndex`, 8);
-            expect(characterClasses).toHaveProperty(`${nonDigitIndexes[0]}.non_digit.endingIndex`, 10);
+            expect(characterClasses).toHaveProperty(`${nonDigitIndexes[0]}.non_digit.lastIndex`, 9);
             expect(characterClasses).toHaveProperty(`${nonDigitIndexes[0]}.non_digit.group`, '\\D');
         });
         it("should add alphanumerics", () => {
             alphanumericIndexes.forEach(alpha => {
                 expect(characterClasses).toHaveProperty(`${alpha}.alphanumeric`);
                 expect(characterClasses).toHaveProperty(`${alpha}.alphanumeric.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${alpha}.alphanumeric.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${alpha}.alphanumeric.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${alpha}.alphanumeric.group`);
             });
             expect(characterClasses).toHaveProperty(`${alphanumericIndexes[0]}.alphanumeric.startingIndex`, 12);
-            expect(characterClasses).toHaveProperty(`${alphanumericIndexes[0]}.alphanumeric.endingIndex`, 14);
+            expect(characterClasses).toHaveProperty(`${alphanumericIndexes[0]}.alphanumeric.lastIndex`, 13);
             expect(characterClasses).toHaveProperty(`${alphanumericIndexes[0]}.alphanumeric.group`, '\\w');
         })
         it("should add non-alphanumerics", () => {
             nonAlphanumericIndexes.forEach(nonAlpha => {
                 expect(characterClasses).toHaveProperty(`${nonAlpha}.non_alphanumeric`);
                 expect(characterClasses).toHaveProperty(`${nonAlpha}.non_alphanumeric.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${nonAlpha}.non_alphanumeric.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${nonAlpha}.non_alphanumeric.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${nonAlpha}.non_alphanumeric.group`);
             });
             expect(characterClasses).toHaveProperty(`${nonAlphanumericIndexes[0]}.non_alphanumeric.startingIndex`, 4);
-            expect(characterClasses).toHaveProperty(`${nonAlphanumericIndexes[0]}.non_alphanumeric.endingIndex`, 6);
+            expect(characterClasses).toHaveProperty(`${nonAlphanumericIndexes[0]}.non_alphanumeric.lastIndex`, 5);
             expect(characterClasses).toHaveProperty(`${nonAlphanumericIndexes[0]}.non_alphanumeric.group`, '\\W');
         });
         it("should add whitespaces", () => {
             whitespaceIndexes.forEach(whitespace => {
                 expect(characterClasses).toHaveProperty(`${whitespace}.whitespace`);
                 expect(characterClasses).toHaveProperty(`${whitespace}.whitespace.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${whitespace}.whitespace.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${whitespace}.whitespace.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${whitespace}.whitespace.group`);
             });
             expect(characterClasses).toHaveProperty(`${whitespaceIndexes[0]}.whitespace.startingIndex`, 34);
-            expect(characterClasses).toHaveProperty(`${whitespaceIndexes[0]}.whitespace.endingIndex`, 36);
+            expect(characterClasses).toHaveProperty(`${whitespaceIndexes[0]}.whitespace.lastIndex`, 35);
             expect(characterClasses).toHaveProperty(`${whitespaceIndexes[0]}.whitespace.group`, '\\s');
         });
         it("should add non-whitespaces", () => {
             nonWhitespaceIndexes.forEach(nonWhitespace => {
                 expect(characterClasses).toHaveProperty(`${nonWhitespace}.non_whitespace`);
                 expect(characterClasses).toHaveProperty(`${nonWhitespace}.non_whitespace.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${nonWhitespace}.non_whitespace.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${nonWhitespace}.non_whitespace.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${nonWhitespace}.non_whitespace.group`);
             });
             expect(characterClasses).toHaveProperty(`${nonWhitespaceIndexes[0]}.non_whitespace.startingIndex`, 2);
-            expect(characterClasses).toHaveProperty(`${nonWhitespaceIndexes[0]}.non_whitespace.endingIndex`, 4);
+            expect(characterClasses).toHaveProperty(`${nonWhitespaceIndexes[0]}.non_whitespace.lastIndex`, 3);
             expect(characterClasses).toHaveProperty(`${nonWhitespaceIndexes[0]}.non_whitespace.group`, '\\S');
         });
         it("should add horizontal tabs", () => {
             horizontalTabIndexes.forEach(horizontalTab => {
                 expect(characterClasses).toHaveProperty(`${horizontalTab}.horizontal_tab`);
                 expect(characterClasses).toHaveProperty(`${horizontalTab}.horizontal_tab.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${horizontalTab}.horizontal_tab.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${horizontalTab}.horizontal_tab.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${horizontalTab}.horizontal_tab.group`);
             });
             expect(characterClasses).toHaveProperty(`${horizontalTabIndexes[0]}.horizontal_tab.startingIndex`, 45);
-            expect(characterClasses).toHaveProperty(`${horizontalTabIndexes[0]}.horizontal_tab.endingIndex`, 47);
+            expect(characterClasses).toHaveProperty(`${horizontalTabIndexes[0]}.horizontal_tab.lastIndex`, 46);
             expect(characterClasses).toHaveProperty(`${horizontalTabIndexes[0]}.horizontal_tab.group`, '\\t');
         });
         it("should add carriage returns", () => {
             carriageReturnIndexes.forEach(carriageReturn => {
                 expect(characterClasses).toHaveProperty(`${carriageReturn}.carriage_return`);
                 expect(characterClasses).toHaveProperty(`${carriageReturn}.carriage_return.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${carriageReturn}.carriage_return.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${carriageReturn}.carriage_return.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${carriageReturn}.carriage_return.group`);
             });
             expect(characterClasses).toHaveProperty(`${carriageReturnIndexes[0]}.carriage_return.startingIndex`, 43);
-            expect(characterClasses).toHaveProperty(`${carriageReturnIndexes[0]}.carriage_return.endingIndex`, 45);
+            expect(characterClasses).toHaveProperty(`${carriageReturnIndexes[0]}.carriage_return.lastIndex`, 44);
             expect(characterClasses).toHaveProperty(`${carriageReturnIndexes[0]}.carriage_return.group`, '\\r');
         });
         it("should add linefeeds", () => {
             lineFeedIndexes.forEach(lineFeed => {
                 expect(characterClasses).toHaveProperty(`${lineFeed}.linefeed`);
                 expect(characterClasses).toHaveProperty(`${lineFeed}.linefeed.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${lineFeed}.linefeed.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${lineFeed}.linefeed.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${lineFeed}.linefeed.group`);
             });
             expect(characterClasses).toHaveProperty(`${lineFeedIndexes[0]}.linefeed.startingIndex`, 15);
-            expect(characterClasses).toHaveProperty(`${lineFeedIndexes[0]}.linefeed.endingIndex`, 17);
+            expect(characterClasses).toHaveProperty(`${lineFeedIndexes[0]}.linefeed.lastIndex`, 16);
             expect(characterClasses).toHaveProperty(`${lineFeedIndexes[0]}.linefeed.group`, '\\n');
         });
         it("should add vertical tabs", () => {
             verticalTabIndexes.forEach(verticalTab => {
                 expect(characterClasses).toHaveProperty(`${verticalTab}.vertical_tab`);
                 expect(characterClasses).toHaveProperty(`${verticalTab}.vertical_tab.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${verticalTab}.vertical_tab.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${verticalTab}.vertical_tab.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${verticalTab}.vertical_tab.group`);
             });
             expect(characterClasses).toHaveProperty(`${verticalTabIndexes[0]}.vertical_tab.startingIndex`, 17);
-            expect(characterClasses).toHaveProperty(`${verticalTabIndexes[0]}.vertical_tab.endingIndex`, 19);
+            expect(characterClasses).toHaveProperty(`${verticalTabIndexes[0]}.vertical_tab.lastIndex`, 18);
             expect(characterClasses).toHaveProperty(`${verticalTabIndexes[0]}.vertical_tab.group`, '\\v');
         });
         it("should add form feeds", () => {
             formFeedIndexes.forEach((formFeed) => {
                 expect(characterClasses).toHaveProperty(`${formFeed}.form_feed`);
                 expect(characterClasses).toHaveProperty(`${formFeed}.form_feed.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${formFeed}.form_feed.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${formFeed}.form_feed.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${formFeed}.form_feed.group`);
             });
             expect(characterClasses).toHaveProperty(`${formFeedIndexes[0]}.form_feed.startingIndex`, 108);
-            expect(characterClasses).toHaveProperty(`${formFeedIndexes[0]}.form_feed.endingIndex`, 110);
+            expect(characterClasses).toHaveProperty(`${formFeedIndexes[0]}.form_feed.lastIndex`, 109);
             expect(characterClasses).toHaveProperty(`${formFeedIndexes[0]}.form_feed.group`, '\\f');
         });
         it("should add backspaces", () => {
             backspaceIndexes.forEach((backspace) => {
                 expect(characterClasses).toHaveProperty(`${backspace}.backspace`);
                 expect(characterClasses).toHaveProperty(`${backspace}.backspace.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${backspace}.backspace.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${backspace}.backspace.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${backspace}.backspace.group`);
             });
             expect(characterClasses).toHaveProperty(`${backspaceIndexes[0]}.backspace.startingIndex`, 105);
-            expect(characterClasses).toHaveProperty(`${backspaceIndexes[0]}.backspace.endingIndex`, 107);
+            expect(characterClasses).toHaveProperty(`${backspaceIndexes[0]}.backspace.lastIndex`, 106);
             expect(characterClasses).toHaveProperty(`${backspaceIndexes[0]}.backspace.group`, '\\b');
         });
         it("should add NUL", () => {
             nulIndexes.forEach((nul) => {
                 expect(characterClasses).toHaveProperty(`${nul}.NUL`);
                 expect(characterClasses).toHaveProperty(`${nul}.NUL.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${nul}.NUL.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${nul}.NUL.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${nul}.NUL.group`);
             });
             expect(characterClasses).toHaveProperty(`${nulIndexes[0]}.NUL.startingIndex`, 102);
-            expect(characterClasses).toHaveProperty(`${nulIndexes[0]}.NUL.endingIndex`, 104);
+            expect(characterClasses).toHaveProperty(`${nulIndexes[0]}.NUL.lastIndex`, 103);
             expect(characterClasses).toHaveProperty(`${nulIndexes[0]}.NUL.group`, '\\0');
         });
         it("should add control characters", () => {
             controlCharacterIndexes.forEach((controlCharacter) => {
                 expect(characterClasses).toHaveProperty(`${controlCharacter}.control_character`);
                 expect(characterClasses).toHaveProperty(`${controlCharacter}.control_character.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${controlCharacter}.control_character.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${controlCharacter}.control_character.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${controlCharacter}.control_character.group`);
             });
             expect(characterClasses).toHaveProperty(`${controlCharacterIndexes[0]}.control_character.startingIndex`, 99);
-            expect(characterClasses).toHaveProperty(`${controlCharacterIndexes[0]}.control_character.endingIndex`, 102);
+            expect(characterClasses).toHaveProperty(`${controlCharacterIndexes[0]}.control_character.lastIndex`, 101);
             expect(characterClasses).toHaveProperty(`${controlCharacterIndexes[0]}.control_character.group`, '\r');
         });
         it("should add hexadecimals", () => {
             hexIndexes.forEach((hex) => {
                 expect(characterClasses).toHaveProperty(`${hex}.hex`);
                 expect(characterClasses).toHaveProperty(`${hex}.hex.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${hex}.hex.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${hex}.hex.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${hex}.hex.group`);
             });
             expect(characterClasses).toHaveProperty(`${hexIndexes[0]}.hex.startingIndex`, 111);
-            expect(characterClasses).toHaveProperty(`${hexIndexes[0]}.hex.endingIndex`, 113);
+            expect(characterClasses).toHaveProperty(`${hexIndexes[0]}.hex.lastIndex`, 114);
             expect(characterClasses).toHaveProperty(`${hexIndexes[0]}.hex.group`, 'E');
         });
         it("should add dots and not dotAlls if dotAll flag is not set", () => {
@@ -196,11 +196,11 @@ describe("getCharacterClasses", () => {
             dotIndexes.forEach((dot) => {
                 expect(characterClasses).toHaveProperty(`${dot}.dot`);
                 expect(characterClasses).toHaveProperty(`${dot}.dot.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${dot}.dot.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${dot}.dot.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${dot}.dot.group`);
             });
             expect(characterClasses).toHaveProperty(`${dotIndexes[0]}.dot.startingIndex`, 110);
-            expect(characterClasses).toHaveProperty(`${dotIndexes[0]}.dot.endingIndex`, 111);
+            expect(characterClasses).toHaveProperty(`${dotIndexes[0]}.dot.lastIndex`, 110);
             expect(characterClasses).toHaveProperty(`${dotIndexes[0]}.dot.group`, '.');
             const groups = Object.values(characterClasses);
             groups.forEach(group => {
@@ -214,11 +214,11 @@ describe("getCharacterClasses", () => {
             dotAllIndexes.forEach((dotAll) => {
                 expect(characterClasses).toHaveProperty(`${dotAll}.dotAll`);
                 expect(characterClasses).toHaveProperty(`${dotAll}.dotAll.startingIndex`);
-                expect(characterClasses).toHaveProperty(`${dotAll}.dotAll.endingIndex`);
+                expect(characterClasses).toHaveProperty(`${dotAll}.dotAll.lastIndex`);
                 expect(characterClasses).toHaveProperty(`${dotAll}.dotAll.group`);
             });
             expect(characterClasses).toHaveProperty(`${dotAllIndexes[0]}.dotAll.startingIndex`, 110);
-            expect(characterClasses).toHaveProperty(`${dotAllIndexes[0]}.dotAll.endingIndex`, 111);
+            expect(characterClasses).toHaveProperty(`${dotAllIndexes[0]}.dotAll.lastIndex`, 110);
             expect(characterClasses).toHaveProperty(`${dotAllIndexes[0]}.dotAll.group`, '.');
             const groups = Object.values(characterClasses);
             groups.forEach(group => {
--- components/getCharacterClasses.js
+++ components/getCharacterClasses.js
@@ -6,28 +6,30 @@ module.exports = {
         const characterClassString = DIGIT + "|" + NON_DIGIT+ "|" + ALPHANUMERIC + "|" + NON_ALPHANUMERIC + "|" + WHITESPACE + "|" + NON_WHITESPACE+ "|" + HORIZONTAL_TAB + "|" + CARRIAGE_RETURN + "|" + LINEFEED + "|" + VERTICAL_TAB + "|" + FORM_FEED + "|" + BACKSPACE + "|" + NUL + "|" + CONTROL_CHARACTER + "|" + HEX + "|" + (flags.includes("s") ? DOTALL : DOT);
         const characterClassRegex = new RegExp(characterClassString, 'g');
         const characterClasses = {};
-
         [...string.matchAll(characterClassRegex)].forEach((regex) => {
             const { groups } = regex;
             const key = findKey(groups);
             let group = groups[key];
             const startingIndex = regex.index;
-            const endingIndex = startingIndex + group.length;
+            const lastIndex = startingIndex + (group.length - 1);
+            let control;
+            let hex;
             if (groups.control_character) {
                 // We need to calculate the value of the character relative to A
                 let codepoint = groups.control_character.slice(2).charCodeAt(0) - "A".charCodeAt(0);
                 // A starts at 1, so 1 must be added to take offset into account
-                group = String.fromCodePoint(codepoint + 1);
+                control = String.fromCodePoint(codepoint + 1);
             } else if (groups.hex) {
                 // Convert string hex to hex number to eval
-                const codepoint = parseInt(groups.hex, 16);
-                group = String.fromCodePoint(codepoint)
+                group = groups.hex.slice(2)
+                const codepoint = parseInt(group, 16);
+                hex = String.fromCodePoint(codepoint)
             }
             characterClasses[startingIndex] = {
                 [key]: {
                     startingIndex,
-                    endingIndex,
-                    group
+                    lastIndex,
+                    group: groups.control_character ? control : groups.hex ? hex : group
                 }
             };
         })
