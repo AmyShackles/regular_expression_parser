966cf5390c52895e4eb0b153944a4650af4d6b7c
Amy Shackles <amyshackles@gmail.com>
2020-07-06 22:01:57 -0700

    Changes endingIndex to lastIndex

--- __tests__/getCaptures.js
+++ __tests__/getCaptures.js
@@ -9,7 +9,7 @@ describe("getCaptures", () => {
     const captures = getCaptures(expression);
     const parenStack = getParenStack(expression);
     let non_capture_groups = {}, capture_groups = {}, named_capture_groups = {};
-    parenStack.forEach(({group, startingIndex, endingIndex}) => {
+    parenStack.forEach(({group, startingIndex, lastIndex}) => {
         const namedCapture = group.match(NAMED_CAPTURE)
         const nonCapture = group.match(NON_CAPTURE);
         const capture = group.match(CAPTURE);
@@ -17,19 +17,19 @@ describe("getCaptures", () => {
             named_capture_groups[startingIndex] = {
                 group: namedCapture['groups']['named_capture_group'],
                 startingIndex,
-                endingIndex
+                lastIndex
             }
         } else if (nonCapture) {
             non_capture_groups[startingIndex] = {
                 group: nonCapture['groups']['non_capture_group'], 
                 startingIndex,
-                endingIndex
+                lastIndex
             }
         } else if (capture) {
             capture_groups[startingIndex] = {
                 group: capture['groups']['capture_group'], 
                 startingIndex,
-                endingIndex
+                lastIndex
             }
         }
     })
@@ -38,36 +38,36 @@ describe("getCaptures", () => {
     named_capture_groups = Object.values(named_capture_groups);
 
     it("should add non-capture groups", () => {
-        non_capture_groups.forEach(({group, startingIndex, endingIndex}) => {
+        non_capture_groups.forEach(({group, startingIndex, lastIndex}) => {
             expect(captures).toHaveProperty(`${startingIndex}.non_capture_group.startingIndex`, startingIndex);
-            expect(captures).toHaveProperty(`${startingIndex}.non_capture_group.endingIndex`, endingIndex);
+            expect(captures).toHaveProperty(`${startingIndex}.non_capture_group.lastIndex`, lastIndex);
             expect(captures).toHaveProperty(`${startingIndex}.non_capture_group.group`, group)
         })
         expect(captures).toHaveProperty(`0.non_capture_group`);
         expect(captures).toHaveProperty(`0.non_capture_group.startingIndex`, 0);
-        expect(captures).toHaveProperty(`0.non_capture_group.endingIndex`, 9);
+        expect(captures).toHaveProperty(`0.non_capture_group.lastIndex`, 9);
         expect(captures).toHaveProperty(`0.non_capture_group.group`, '(^|\\s)');
     });
     it("should add named capture groups", () => {
-        named_capture_groups.forEach(({group, startingIndex, endingIndex}) => {
+        named_capture_groups.forEach(({group, startingIndex, lastIndex}) => {
             expect(captures).toHaveProperty(`${startingIndex}.named_capture_group.startingIndex`, startingIndex);
-            expect(captures).toHaveProperty(`${startingIndex}.named_capture_group.endingIndex`, endingIndex);
+            expect(captures).toHaveProperty(`${startingIndex}.named_capture_group.lastIndex`, lastIndex);
             expect(captures).toHaveProperty(`${startingIndex}.named_capture_group.group`, group)
         })
         expect(captures).toHaveProperty(`32.named_capture_group.startingIndex`, 32);
-        expect(captures).toHaveProperty(`32.named_capture_group.endingIndex`, 61);
+        expect(captures).toHaveProperty(`32.named_capture_group.lastIndex`, 61);
         expect(captures).toHaveProperty(`32.named_capture_group.group`, '12(?<another>nesting)3');
         expect(captures).toHaveProperty(`32.named_capture_group.name`, 'num');
     });
     it("should add unnamed capture groups", () => {
-        capture_groups.forEach(({group, startingIndex, endingIndex}) => {
+        capture_groups.forEach(({group, startingIndex, lastIndex}) => {
             expect(captures).toHaveProperty(`${startingIndex}.capture_group.startingIndex`, startingIndex);
-            expect(captures).toHaveProperty(`${startingIndex}.capture_group.endingIndex`, endingIndex);
+            expect(captures).toHaveProperty(`${startingIndex}.capture_group.lastIndex`, lastIndex);
             expect(captures).toHaveProperty(`${startingIndex}.capture_group.group`, group);
 
         })
         expect(captures).toHaveProperty(`10.capture_group.startingIndex`, 10);
-        expect(captures).toHaveProperty(`10.capture_group.endingIndex`, 14);
+        expect(captures).toHaveProperty(`10.capture_group.lastIndex`, 14);
         expect(captures).toHaveProperty(`10.capture_group.group`, 'ABC');
     });
 })
--- components/getCaptures.js
+++ components/getCaptures.js
@@ -4,16 +4,16 @@ const { getParenStack } = require("../utils/getParenStack.js");
 module.exports = {
     getCaptures: (string) => {
         let captures = {};
-        getParenStack(string).forEach(({group, startingIndex, endingIndex}) => {
+        getParenStack(string).forEach(({group, startingIndex, lastIndex}) => {
             const named_capture = group.match(NAMED_CAPTURE);
             const non_capture = group.match(NON_CAPTURE);
             const capture = group.match(CAPTURE);
     
             if (named_capture || non_capture || capture) {
                 captures[startingIndex] = {
-                    ...(named_capture ? { named_capture_group: { group: named_capture['groups']['named_capture_group'], startingIndex, endingIndex, name: named_capture['groups']['name'] }} : {}),
-                    ...(non_capture ? { non_capture_group: { group: non_capture['groups']['non_capture_group'], startingIndex, endingIndex}} : {}),
-                    ...(capture ? { capture_group: { group: capture['groups']['capture_group'], startingIndex, endingIndex }} : {})
+                    ...(named_capture ? { named_capture_group: { group: named_capture['groups']['named_capture_group'], startingIndex, lastIndex, name: named_capture['groups']['name'] }} : {}),
+                    ...(non_capture ? { non_capture_group: { group: non_capture['groups']['non_capture_group'], startingIndex, lastIndex}} : {}),
+                    ...(capture ? { capture_group: { group: capture['groups']['capture_group'], startingIndex, lastIndex }} : {})
                 }
             }
         });
