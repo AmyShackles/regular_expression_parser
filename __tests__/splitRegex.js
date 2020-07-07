578c70aaa8f76a64cdb5ec7e24e946011ef05825
Amy Shackles <amyshackles@gmail.com>
2020-07-06 22:12:03 -0700

    Adds tests for splitRegex

--- __tests__/splitRegex.js
+++ __tests__/splitRegex.js
@@ -1,3 +1,12 @@
-// const { splitRegex } = require('../components/splitRegex.js');
+const { splitRegex } = require('../components/splitRegex.js');
 
-test.todo("splitRegex");
+describe("splitRegex", () => {
+    const reg = /(?:(^|\s))(ABC) as easy as (123), \1, \2/gm;
+
+    it("should return a string representation of a regular expression", () => {
+        expect(splitRegex(reg)).toEqual(expect.objectContaining({ expression: "(?:(^|\\s))(ABC) as easy as (123), \\1, \\2"}))
+    })
+    it("should return a string representation of flags set", () => {
+        expect(splitRegex(reg)).toEqual(expect.objectContaining({flags: "gm"}))
+    })
+})
